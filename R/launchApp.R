e96566299708763429e4618cd93728a076800fbb
Pablo Ezquerra <50584096+PEzquerra@users.noreply.github.com>
2021-02-11 18:59:38 -0300

    Update launchApp.R

--- R/launchApp.R
+++ R/launchApp.R
@@ -1,3 +1,15 @@
+
+#' launches the shinyAppDemo app
+#'
+#' @export twitter.data
+#'
+#' @return shiny application object
+#'
+#' @example \dontrun {launchApp()}
+#'
+#' @import shiny
+#'
+
 twitter.data <- function() {
   shinyApp(ui = shinyAppUI, server = shinyAppServer)
 }
